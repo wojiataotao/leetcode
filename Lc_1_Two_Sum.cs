c8e1234403003d185e0821b51f0078013acc2763
yazhen sun <yzsun@yazhens-MacBook-Pro.local>
2017-03-01 00:46:21 -0800

    1st play

---
+++ Lc_1_Two_Sum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        if(nums.Length<2) return new int[]{-1, -1}; 
+        
+        int[] res={-1, -1}; 
+        Dictionary<int, int> map=new Dictionary<int, int>(); 
+        
+        for(int i=0; i<nums.Length; i++){
+            if(map.ContainsKey(target-nums[i])){
+                res[0]=map[target-nums[i]]; 
+                res[1]=i; 
+                break; 
+            }
+            map[nums[i]]=i; 
+        }
+        return res; 
+    }
+}
